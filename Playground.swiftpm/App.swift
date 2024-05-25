e2dcd86dba9fe147267cce1dd09aeb895b9ae565
noppefoxwolf <noppelabs@gmail.com>
2024-05-25 18:02:01 +0900

    Update App.swift

--- Playground.swiftpm/App.swift
+++ Playground.swiftpm/App.swift
@@ -29,15 +29,17 @@ final class PageViewController: Pager.PageViewController, Pager.PageTabBarDataSo
         navigationItem.title = "Pager Example"
         navigationItem.setBottomPalette(pageTabBar)
         
+        let stepper = UIStepper(frame: .null, primaryAction: UIAction { action in
+            let stepper = action.sender as! UIStepper
+            self.items = (0..<Int(stepper.value.rounded())).map({ "\($0)" })
+            self.reloadData()
+        })
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: stepper)
+        
         reloadData()
     }
     
-    var items = [
-        "Following",
-        "Local",
-        "Global",
-        "#ios_developers",
-    ]
+    var items: [String] = []
     
     func barItem(for bar: PageTabBar, at index: Int) -> any PageTabBarItem {
         DefaultPageTabBarItem(title: items[index])
@@ -48,7 +50,7 @@ final class PageViewController: Pager.PageViewController, Pager.PageTabBarDataSo
     }
     
     func viewController(for pageViewController: Pager.PageViewController, at index: Int) -> UIViewController? {
-        UIHostingController(rootView: Text(items[index]))
+        ChildViewController(text: items[index])
     }
 }
 
@@ -67,5 +69,52 @@ extension UINavigationItem {
         if responds(to: _setButtomPaletteSelector) {
             perform(_setButtomPaletteSelector, with: palette)
         }
+    }
+}
+
+
+class ChildViewController: UIViewController {
+    let label: UILabel = UILabel()
+    let button: UIButton = UIButton(configuration: .filled())
+    let text: String
+    
+    init(text: String) {
+        self.text = text
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        label.text = text
+        button.configuration?.title = "Button"
+        
+        let stackView = UIStackView(
+            arrangedSubviews: [
+                label,
+                button
+            ]
+        )
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(
+                equalTo: view.centerYAnchor
+            ),
+            stackView.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            view.trailingAnchor.constraint(
+                equalTo: stackView.safeAreaLayoutGuide.trailingAnchor,
+                constant: 20
+            ),
+        ])
     }
 }
