769c76707881577745655e4e4eae84927762b389
noppe <noppelabs@gmail.com>
2024-11-11 21:14:05 +0900

    ref

--- Sources/Pager/PageViewController.swift
+++ Sources/Pager/PageViewController.swift
@@ -97,11 +97,6 @@ open class PageViewController: UICollectionViewController {
         hostedViewControllers = []
         collectionView.reloadData()
         pageTabBar.reloadData()
-        // workaround:
-        // https://stackoverflow.com/a/30668519
-        pageTabBar.performBatchUpdates(nil) { [weak pageTabBar] _ in
-            pageTabBar?.invalidateIndicatorLayout()
-        }
     }
 }
 
@@ -115,36 +110,3 @@ extension PageViewController: PageTabBarDelegate {
     }
 }
 
-extension UICollectionViewLayout {
-    static func paging(
-        visibleItemsInvalidationHandler: @escaping
-        NSCollectionLayoutSectionVisibleItemsInvalidationHandler
-    ) -> UICollectionViewCompositionalLayout {
-        let configuration = UICollectionViewCompositionalLayoutConfiguration()
-        configuration.scrollDirection = .horizontal
-        configuration.contentInsetsReference = .automatic
-        return UICollectionViewCompositionalLayout(
-            sectionProvider: { section, environment in
-                let itemSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .fractionalHeight(1)
-                )
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                let groupSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .fractionalHeight(1)
-                )
-                let group = NSCollectionLayoutGroup.horizontal(
-                    layoutSize: groupSize,
-                    repeatingSubitem: item,
-                    count: 1
-                )
-                let section = NSCollectionLayoutSection(group: group)
-                section.orthogonalScrollingBehavior = .paging
-                section.visibleItemsInvalidationHandler = visibleItemsInvalidationHandler
-                return section
-            },
-            configuration: configuration
-        )
-    }
-}
