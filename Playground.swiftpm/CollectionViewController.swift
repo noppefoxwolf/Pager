7e1425e00818d0bce5a47945a73718bf529182f4
noppe <noppelabs@gmail.com>
2024-12-06 01:10:36 +0900

    Update CollectionViewController.swift

--- Playground.swiftpm/CollectionViewController.swift
+++ Playground.swiftpm/CollectionViewController.swift
@@ -1,42 +1,43 @@
 import UIKit
 
 enum Section: Int {
-  case items
+    case items
 }
 
 struct Item: Hashable {
-  let id: UUID = UUID()
+    let id: UUID = UUID()
 }
 
 final class CollectionViewController: UICollectionViewController {
-  let layout = UICollectionViewCompositionalLayout.list(using: .init(appearance: .plain))
-  
-  let cellRegistration = UICollectionView.CellRegistration(
-    handler: { (cell: UICollectionViewListCell, indexPath, item: Item) in
-      var contentConfiguration = cell.defaultContentConfiguration()
-      contentConfiguration.text = "Hello, World!"
-      cell.contentConfiguration = contentConfiguration
+    let layout = UICollectionViewCompositionalLayout.list(using: .init(appearance: .plain))
+    
+    let cellRegistration = UICollectionView.CellRegistration(
+        handler: { (cell: UICollectionViewListCell, indexPath, item: Item) in
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = "Hello, World!"
+            cell.contentConfiguration = contentConfiguration
+        }
+    )
+    
+    lazy var dataSource = UICollectionViewDiffableDataSource<Section, Item>(
+        collectionView: collectionView,
+        cellProvider: { [unowned self] (collectionView, indexPath, item) in
+            collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+        }
+    )
+    
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        collectionView.dataSource = dataSource
+        
+        snapshot.appendSections([.items])
+        let items = (0..<100).map({ _ in Item() })
+        snapshot.appendItems(items, toSection: .items)
+        
+        dataSource.apply(snapshot)
     }
-  )
-  
-  lazy var dataSource = UICollectionViewDiffableDataSource<Section, Item>(
-    collectionView: collectionView,
-    cellProvider: { [unowned self] (collectionView, indexPath, item) in
-      collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
-    }
-  )
-  
-  var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    collectionView.setCollectionViewLayout(layout, animated: false)
-    collectionView.dataSource = dataSource
-    
-    snapshot.appendSections([.items])
-    snapshot.appendItems([Item(), Item(), Item()], toSection: .items)
-    
-    dataSource.apply(snapshot)
-  }
 }
