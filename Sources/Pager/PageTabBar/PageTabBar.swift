6fd55fceeaf5291cf2441b8a092ab647bfbf9386
noppe <noppelabs@gmail.com>
2025-11-30 19:36:02 +0900

    Update PageTabBar.swift

--- Sources/Pager/PageTabBar/PageTabBar.swift
+++ Sources/Pager/PageTabBar/PageTabBar.swift
@@ -25,6 +25,7 @@ public final class PageTabBar: UICollectionView {
         
         feedbackGenerator.prepare()
         
+        indicatorView.translatesAutoresizingMaskIntoConstraints = true
         addSubview(indicatorView)
     }
     
