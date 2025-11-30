aed05cc811e2f531c0b2f7a0e6f3be432888d0ef
noppe <noppelabs@gmail.com>
2025-11-30 14:30:03 +0900

    Update PageItemViewController.swift

--- Sources/Pager/PageItemViewController.swift
+++ Sources/Pager/PageItemViewController.swift
@@ -8,9 +8,12 @@ final class PageItemViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
         viewController.willMove(toParent: self)
         addChild(viewController)
         view.addSubview(viewController.view)
@@ -22,10 +25,6 @@ final class PageItemViewController: UIViewController {
             viewController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
             view.rightAnchor.constraint(equalTo: viewController.view.rightAnchor),
         ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     override func contentScrollView(for edge: NSDirectionalRectEdge) -> UIScrollView? {
