2b021a705c158d02e9acbdf52df309cead00927f
noppe <noppelabs@gmail.com>
2025-12-22 00:43:21 +0900

    update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/noppefoxwolf/CollectionViewDistributionalLayout", from: "0.0.2")
+        .package(
+            url: "https://github.com/noppefoxwolf/CollectionViewDistributionalLayout",
+            from: "0.0.4"
+        )
     ],
     targets: [
         .target(
