b24448682f75cea2a826fdecff3031537efc4bd5
noppe <noppelabs@gmail.com>
2025-12-21 22:54:34 +0900

    Update Page.swift

--- Sources/Pager/Page.swift
+++ Sources/Pager/Page.swift
@@ -5,6 +5,18 @@ public final class Page: Identifiable {
     public let title: String
     public let viewControllerProvider: @MainActor @Sendable (Page) -> UIViewController
     
+    private var _viewController: UIViewController? = nil
+    
+    @MainActor
+    public var viewController: UIViewController {
+        if let _viewController {
+            return _viewController
+        }
+        let newViewController = viewControllerProvider(self)
+        _viewController = newViewController
+        return newViewController
+    }
+    
     public init(
         id: String,
         title: String,
